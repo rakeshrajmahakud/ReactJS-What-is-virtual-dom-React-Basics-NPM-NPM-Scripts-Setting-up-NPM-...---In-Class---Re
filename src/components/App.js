37eb8eed699cdba6c96fb7b405019bb189a7d433
rakeshrajMahakud <rakeshraj.mahakud03@gmail.com>
2022-06-24 01:07:46 +0530

    assignment completed by rakeshraj

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
-// import React, {Component, useState} from "react";
+import React, {Component, useState} from "react";
 import '../styles/App.css';
 
 const App = () => {
   return (
     <div id="main">
     <h1 data-ns-test="project-name">Resource Website</h1>
-    <p data-ns-test="project-description">In this website ,user can get best website resources like design and dev related tools and technologies</p>
+    <p data-ns-test="project-description">I want to make my own resource website where anyone can find great resources</p>
     </div>
   );
 }
 
 
 export default App;
-
